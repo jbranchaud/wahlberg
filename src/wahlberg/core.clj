e97e49bdf86d1b29b28f7c2e7373b75997b1bd92
jbranchaud <jbranchaud@gmail.com>
2015-04-25 18:59:02 -0500

    Add an MVP version of markov chain text generation.

--- src/wahlberg/core.clj
+++ src/wahlberg/core.clj
@@ -1,7 +1,103 @@
 (ns wahlberg.core
   (:gen-class))
 
+;
+; read content from data source (file, http, etc.)
+;
+(defn get-source
+  "Read in the source content from the file with the given name"
+  [filename]
+  (slurp filename))
+
+;
+; filter out all bad characters (", -, etc.)
+;
+(defn filter-input
+  "Remove all the junk characters from the input"
+  [input]
+  (clojure.string/replace input #"" ""))
+
+;
+; split everything on whitespace
+;
+(defn tokenize-words
+  "Tokenize the given string based primarily on whitespace"
+  [input]
+  (clojure.string/split input #"[\s]+"))
+;(tokenize-words "these are some  words   to\tsplit\non.")
+
+;
+; organize into frequency map
+;
+(defn generate-frequency-map
+  "Generate a map of word frequencies as they follow one another"
+  [words]
+  (reduce
+    (fn [freq-map word-group]
+      (let [[lead follow] word-group
+            lead-map (or (get freq-map lead) {})
+            follow-val (or (get lead-map follow) 0)]
+        (assoc freq-map lead (assoc lead-map follow (inc follow-val)))))
+    {}
+    (partition 2 1 words)))
+;(generate-frequency-map ["this" "is" "this" "is" "this" "is" "a" "vector" "of" "this" "words"])
+
+;
+; convert into probability map
+;
+(defn generate-probability-map
+  "Generate a map of word probabilities as they follow one another"
+  [freq-map]
+  (reduce
+    (fn [prob-map [lead follow-map]]
+      (let [lead-freq (reduce + (vals follow-map))]
+        (assoc prob-map lead (reduce
+                               (fn [probs [follow follow-val]]
+                                 (assoc probs follow (/ follow-val lead-freq)))
+                               {}
+                               follow-map))))
+    {}
+    freq-map))
+;(generate-probability-map (generate-frequency-map ["this" "is" "this" "is" "this" "is" "a" "vector" "of" "this" "words"]))
+
+(defn ends-in-punctuation
+  "Determines if the given string ends in punctuation"
+  [word]
+  (= (last word) \.))
+
+(defn get-next-word
+  "Randomly pick the next word based on current word"
+  [probability-map word]
+  (let [follow-words (get probability-map word)
+        rand-num (rand)]
+    (if (nil? follow-words)
+      "the"
+      (loop [rolling-sum 0
+             index 0]
+        (let [new-sum (+ rolling-sum (get follow-words (nth (keys follow-words) index)))]
+          (if (< rand-num new-sum)
+            (nth (keys follow-words) index)
+            (recur new-sum (inc index))))))))
+
+(defn generate-text
+  "Generate random text based on a word probability vector"
+  [probability-map]
+  (let [random-word (rand-nth (keys probability-map))]
+    (println
+      (clojure.string/join " "
+        (loop [curr-word random-word
+               words (vector)]
+          (if (ends-in-punctuation curr-word)
+            (conj words curr-word)
+            (recur (get-next-word probability-map curr-word) (conj words curr-word))))))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (->> "assets/war_and_peace.txt"
+       (get-source)
+       (filter-input)
+       (tokenize-words)
+       (generate-frequency-map)
+       (generate-probability-map)
+       (generate-text)))
--- test/wahlberg/core_test.clj
+++ test/wahlberg/core_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [wahlberg.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest tokenize-words-test
+  (testing "Tokenize words can tokenize based on whitespace"
+    (is (tokenize-words "these are some words")
+        ["these" "are" "some" "words"])
+    (is (tokenize-words "these  are  some    words")
+        ["these" "are" "some" "words"])
+    (is (tokenize-words "these  are\tsome\nwords")
+        ["these" "are" "some" "words"])))
+
+(deftest generate-frequency-map-test
+  (testing "Generates a frequency map from the vector of words"
+    (is (= (generate-frequency-map ["this" "is" "this" "is" "this" "is" "the" "way" "this" "will" "be"]) { "this" { "is" 3 "will" 1 } "is" { "this" 2 "the" 1 } "the" { "way" 1 } "way" { "this" 1 } "will" { "be" 1 } "be" { "" 1 } } ))))
+
+(deftest generate-probability-map-test
+  (testing "Generates a probability map from the frequency map"
+    ))
