f30e9724a7a0aa5398f4d16695cdd83be266b125
jbranchaud <jbranchaud@gmail.com>
2015-08-10 21:32:41 -0500

    Add a source text to work with during development.

--- src/wahlberg/core.clj
+++ src/wahlberg/core.clj
@@ -1,138 +1,144 @@
 (ns wahlberg.core
   (:gen-class))
 
-;
-; Read content from data source (file, http, etc.)
-;
-(defn get-source
-  "Read in the source content from the file with the given name"
-  [filename]
-  (slurp filename))
+(def source-text
+  "“He called me a wise guy and punched me in the nose.
+  ‘What are you, a wise guy?’ he said, and knocked me flat on my ass.
+  Pow! Just like that.
+  I’m not kidding.”
 
-;
-; Remove all the junk characters from the input
-;
-(defn filter-input
-  "Remove all the junk characters from the input"
-  [input]
-  (clojure.string/replace input #"" ""))
+  “I know you’re not kidding,” Yossarian said.
+  “But why did he do it?”
 
-;
-; Tokenize the given string based primarily on whitespace
-;
-(defn tokenize-words
-  "Tokenize the given string based primarily on whitespace"
-  [input]
-  (clojure.string/split input #"[\s]+"))
+  “How should I know why he did it?” Doc Daneeka retorted with annoyance.
 
-;
-; Generate a map of word frequencies as they follow one another
-;
-(defn generate-frequency-map
-  "Generate a map of word frequencies as they follow one another"
-  [words]
-  (reduce
-    (fn [freq-map word-group]
-      (let [[lead follow] word-group
-            lead-map (or (get freq-map lead) {})
-            follow-val (or (get lead-map follow) 0)]
-        (assoc freq-map lead (assoc lead-map follow (inc follow-val)))))
-    {}
-    (partition 2 1 (conj words ""))))
+  “Maybe it had something to do with Saint Anthony?”
 
-(defn generate-tuple-frequency-map
-  "Generate a frequency map of word tuples to their following word"
-  [words]
-  (reduce
-    (fn [freq-map word-group]
-      (let [lead (butlast word-group)
-            follow (last word-group)
-            lead-map (or (get freq-map lead) {})
-            follow-val (or (get lead-map follow) 0)]
-        (assoc freq-map lead (assoc lead-map follow (inc follow-val)))))
-    {}
-    (partition 3 1 words)))
+  Doc Daneeka looked at Yossarian blankly.
+  “Saint Anthony?” he asked with astonishment.
+  “Who’s Saint Anthony?”
 
-(defn generate-nary-frequency-map
-  "Generate a frequency map of n-ary word tuples to their following word"
-  [words n]
-  (reduce
-    (fn [freq-map word-group]
-      (let [lead (butlast word-group)
-            follow (last word-group)
-            lead-map (or (get freq-map lead) {})
-            follow-val (or (get lead-map follow) 0)]
-        (assoc freq-map lead (assoc lead-map follow (inc follow-val)))))
-    {}
-    (partition (inc n) 1 words)))
+  “How should I know?” answered Chief White Halfoat, staggering inside the tent just then with a bottle of whiskey cradled in his arm and sitting himself down pugnaciously between the two of them.
 
-;
-; Generate a map of word probabilities as they follow one another
-;
-(defn generate-probability-map
-  "Generate a map of word probabilities as they follow one another"
-  [freq-map]
-  (reduce
-    (fn [prob-map [lead follow-map]]
-      (let [lead-freq (reduce + (vals follow-map))]
-        (assoc prob-map lead (reduce
-                               (fn [probs [follow follow-val]]
-                                 (assoc probs follow (/ follow-val lead-freq)))
-                               {}
-                               follow-map))))
-    {}
-    freq-map))
+  Doc Daneeka rose without a word and moved outside the tent, his back bowed by the compact kit of inustices that was his perpetual burden.
+  He could not bear the company of his roommate.
 
-;
-; Determines if the given string ends in punctuation
-;
-(defn ends-in-punctuation
-  "Determines if the given string ends in punctuation"
-  [word]
-  (= (last word) \.))
+  Chief White Halfoat thought he was crazy.
+  “I don’t know what’s the matter with that guy,” he observed reproachfully.
+  “He’s got no brains, that’s what’s the matter with him.
+  If he had any brains he’d grab a shovel and start digging.
+  Right here in the tent, he’d start digging, right under my cot.
+  He’d strike oil in no time.
+  Don’t he know how that enlisted man struck oil with a shovel back in the States? Didn’t he ever hear what happened to that kid – what was the name of that rotten rat bastard pimp of a snotnose back in Colorado?”
 
-;
-; Randomly pick the next word based on current word
-;
-(defn get-next-word
-  "Randomly pick the next word based on current word"
-  [probability-map word]
-  (let [follow-words (get probability-map word)
-        rand-num (rand)]
-    (if (nil? follow-words)
-      "the"
-      (loop [rolling-sum 0
-             index 0]
-        (let [new-sum (+ rolling-sum (get follow-words (nth (keys follow-words) index)))]
-          (if (< rand-num new-sum)
-            (nth (keys follow-words) index)
-            (recur new-sum (inc index))))))))
+  “Wintergreen.”
 
-;
-; Generate random text based on a word probability vector
-;
-(defn generate-text
-  "Generate random text based on a word probability vector"
-  [probability-map]
-  (let [random-word (rand-nth (keys probability-map))]
-    (println
-      (clojure.string/join " "
-        (loop [curr-word random-word
-               words (vector)]
-          (if (ends-in-punctuation curr-word)
-            (conj words curr-word)
-            (recur (get-next-word probability-map curr-word) (conj words curr-word))))))))
+  “Wintergreen.”
+
+  “He’s afraid,”Yossarian explained.
+
+  “Oh, no.
+  Not Wintergreen.” Chief White Halfoat shook his head with undisguised admiration.
+  “That stinking little punk wise-guy son of a bitch ain’t afraid of nobody.”
+
+  “Doc Daneeka’s afraid.
+  That’s what’s the matter with him.”
+
+  “What’s he afraid of?”
+
+  “He’s afraid of you,” Yossarian said.
+  “He’s afraid you’re going to die of pneumonia.”
+
+  “He’d better be afraid,” Chief White Halfoat said.
+  A deep, low laugh tumbled through his massive chest.
+  “I will, too, the first chance I get.
+  You just wait and see.”
+
+  Chief White Halfoat was a handsome, swarthy Indian from Oklahoma with a heavy, hard-boned face and tousled black hair, a half-blooded Creek from Enid who, for occult reasons of his own, had made up his mind to die of pneumonia.
+  He was a glowering, vengeful, disillusioned Indian who hated foreigners with names like Cathcart, Korn, Black and Havermeyer and wished they’d all go back to where their lousy ancestors had come from.
+
+  “You wouldn’t believe it, Yossarian,” he ruminated, raising his voice deliberately to bait Doc Daneeka, “but this used to be a pretty good country to live in before they loused it up with their goddamn piety.”
+
+  Chief White Halfoat was out to revenge himself upon the white man.
+  He could barely read or write and had been assigned to Captain Black as assistant intelligence officer.
+
+  “How could I learn to read or write?” Chief White Halfoat demanded with simulated belligerence, raising his voice again so that Doc Daneeka would hear.
+  “Every place we pitched our tent, they sank an oil well.
+  Every time they sank a well, they hit oil.
+  And every time they hit oil, they made us pack up our tent and go someplace else.
+  We were human divining rods.
+  Our whole family had a natural affinity for petroleum deposits, and soon every oil company in the world had technicians chasint us around.
+  We were always on the move.
+  It was one hell of a way to bring a child up, I can tell you.
+  I don’t think I ever spent more than a week in one place.”
+
+  His earliest memory was of a geologist.
+
+  “Every time another White Halfoat was born,” he continued, “the stock market turned bullish.
+  Soon whole drilling crews were following us around with all their equipment just to get the jump on each other.
+  Companies began to merge just so they could cut down on the number of people they had to assign to us.
+  But the crowd in back of us kept growing.
+  We never got a good night’s sleep.
+  When we stopped, they stopped.
+  When we moved, they moved, chuckwagons, bulldozers, derricks, generators.
+  We were a walking business boom, and we began to receive invitations from some of the best hotels just for the amount of business we would drag into town wiht us.
+  Some of those invitations were mighty generous, but we couldn’t accept any because we were Indians and all the best hotels that were inviting us wouldn’t accept Indians as gusts.
+  Racial prejudice is a terrible thing, Yossarian.
+  It really is.
+  It’s a terrible thing to treat a decent, loyal Indian like a nigger, kike, wop, or spic.” Chief White Halfoat nodded slowly with conviction.
+
+  “Then, Yossarian, it finally happened – the beginning of the end.
+  They began to follow us around from in front.
+  They would try to guess where we were going to stop next and would begin drilling before we even got there, so we couldn’t even stop.
+  As soon as we’d begin to unroll our blankets, they would kick us off.
+  They had confidence in us.
+  They wouldn’t even wait to strike oil before they kicked us off.
+  We were so tired we almost didn’t care the day our time ran out.
+  One morning we found ourselves completely surrounded by oilmen waiting for us to come their way so they could kick us off.
+  Everywhere you looked there was an oilman on a ridge, waiting there like Indians getting ready to attack.
+  It was the end.
+  We couldn’t stay where we were because we had just been kicked off.
+  And there was no place left for us to go.
+  Only the Army saved me.
+  Luckily, the war broke out just in the nick of time, and a draft board picked me right up out of the middle and put me down safely in Lowery Field, Colorado.
+  I was the only survivor.”
+
+  Yossarian knew he was lying, but did not interrupt as Chief White Halfoat went on to claim that he had never heard from his parents again.
+  That didn’t bother him too much, though, for he had only their word for it that they were his parents, and since they had lied to him about so many other things, they could just as well have been lying to him about that too.
+  He was much better acquainted with the fate of a tribe of first cousins who had wandered away north in a diversionary movement and pushed inadvertently into Canada.
+  When they tried to return, they were stopped at th and a draft board picked me right up out of the middle and put me down safely in Lowery Field, Colorado.
+  I was the only survivor.”
+
+  Yossarian knew he was lying, but did not interrupt as Chief White Halfoat went on to claim that he had never heard from his parents again.
+  That didn’t bother him too muc be down safely in Lowery Field, Colorado.
+  I was the only survivo parents again.
+  That didn’t bother him too much, though, for he had only their word for it that they were his parents, and since they had lied to him about so many other things, they could juste had only their word for it that they were his parents, and sinCa just as well have been lying to him about that too.
+  He was much better acquainted with the fate of a tribe of first cousis nadvertently into Canada.
+  When they tried to return, they were stopped at th and a draft board picked me right up out of the middle and put me down safely in Lowery Field, Colorado.
+  I was thâeld, Colorado.
+  I was the only survivo parents again.
+  That didn’t bother him too much, though, for he had only their word for it that they were his parents, and since they had lied to him about so many other things, they could juste had only their word for it that they were his parents, and sinCa just as well have been lying to him about that too.
+  He was much better acquainted with the fate of a tribe y other things, they could juste had only their word for it that they were his parents, and sinCa just as well have been lying to him about that too.
+  He was much better acquainted with the fate of a tribedofirst cousis nadvertently into Canada.
+  When they tried to return, they were stopped at th and a draft board picked me right up out of the middle and put me down safely in Lowery Field, Colorado.
+  I was thâparents, and sinCa just as well have been lying to him about that too.
+  He was much better acquainted with the fate of a tribedofirst cousis nadvertently into Canada.
+  When they tried to return, they were stopped at th and a draft board picked me right up out of the middle and put me down safely in Lowery Field, Colorado.
+  I was thâparents, and sinCa just as well have been lying to him about that too.
+  He was much better acquainted with the fate of a tribedofirst cousis nadvertently into Canada.
+  When they tried to return, they were stopped at th and a draft board picked me right up out of the middle and put me down safely in Lowery Field, Colorado.
+  I was thâparents, and sinCa justore missions.
+  Orr would be crazy to fly more missions and sane if he didn’t, but if he was sane he had to fly them.
+  If he flew them he was crazy and didn’t have to; but if he didn’t want to he was sane and had to.
+  Yossarian was moved very deeply by the absolute simplicity of this clause of Catch-22 and let out a respectful whistle.
+
+  “That’s some catch, that Catch-22,” he observed.
+
+  “It’s the best there is,” Doc Daneeka agreed.")
 
 ;
 ; Main
 ;
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (->> "assets/war_and_peace.txt"
-       (get-source)
-       (filter-input)
-       (tokenize-words)
-       (generate-frequency-map)
-       (generate-probability-map)
-       (generate-text)))
+  (println "Hello, World!"))
